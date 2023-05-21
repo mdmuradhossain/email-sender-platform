11b743edca19e61f58bf17bc3b02d2e3105660be
mdmuradhossain <mdmuradhossain33@gmail.com>
2023-05-21 21:21:47 +0600

    repo created

---
+++ src/main/java/io/murad/email_sender_platform/EmailSenderPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.murad.email_sender_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSenderPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/murad/email_sender_platform/EmailSenderPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.murad.email_sender_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSenderPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
